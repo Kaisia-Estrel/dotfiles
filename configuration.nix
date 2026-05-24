125c3101eb10a49c68b8674bf96e85f20c965c03
Trouble-Truffle <perigordtruffle7318@gmail.com>
2026-05-24 10:16:33 +0800

    feat: add pwfeedback to sudo

--- configuration.nix
+++ configuration.nix
@@ -489,7 +489,9 @@
   };
 
   security.rtkit.enable = true;
-
+  security.sudo.extraConfig = ''
+    Defaults pwfeedback
+  '';
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
